9789bc6343811dabe25c8e10ff04435396f3b516
michaelmackenzie <michaelmackenzie@u.northwestern.edu>
2019-09-04 10:56:52 -0500

    Added 2D linear correlation plotting

--- tmva_training/scripts/plot_tmva_tree.C
+++ tmva_training/scripts/plot_tmva_tree.C
@@ -7,20 +7,37 @@ double yMin_ = 0.; //plotting domain for 2D
 double yMax_ = 100.;
 int    binsy_ = 100; //number of histogram bins
 int    print_ = 0; //print canvases
-int setSilent_ = 0; //sets to batch mode for canvas printing
+int    setSilent_ = 0; //sets to batch mode for canvas printing
+int    new_training_ = 1; //new root trainings have different file structure
 
-int plot_tmva_tree(const char* file = "../CWoLa_training_background_3.root", double mva_cut = 0.,
+int plot_tmva_tree(const char* file = "CWoLa_training_background_3", double mva_cut = 0.,
 		   int category = -1) {
 
   if(setSilent_) gROOT->SetBatch(kTRUE);
   else gROOT->SetBatch(kFALSE);
 
   TString fname = file;
-  TFile* f = new TFile(file,"READ");
+  fname = "../" + fname;
+  fname += "/";
+  fname += file;
+  fname += ".root";
+    
+  TFile* f = new TFile(fname.Data(),"READ");
   if(!f) {
     printf("File not found\n");
     return 1;
   }
+
+  if(new_training_) {
+    TString foldername = "tmva_";
+    foldername += file;
+    f = (TFile*) f->Get(foldername.Data());
+  }
+  if(!f)  {
+    printf("Folder in file not found\n");
+    return 2;
+  }
+  
   TTree *test_tree  = (TTree*) f->Get("TestTree");
   TTree *train_tree = (TTree*) f->Get("TrainTree");
   if(!test_tree || !train_tree) {
@@ -102,12 +119,28 @@ int stack_tmva_tree(const char* file = "../CWoLa_training_background_3.root", do
 		  int  plot_train = 1)  {
 
   if(setSilent_) gROOT->SetBatch(kTRUE);
-  TFile* f = new TFile(file,"READ");
+
+  TString fname = file;
+  fname = "../" + fname;
+  fname += "/";
+  fname += file;
+  fname += ".root";
+    
+  TFile* f = new TFile(fname.Data(),"READ");
   if(!f) {
     printf("File not found\n");
     return 1;
   }
-  TString fname = file;
+
+  if(new_training_) {
+    TString foldername = "tmva_";
+    foldername += file;
+    f = (TFile*) f->Get(foldername.Data());
+  }
+  if(!f)  {
+    printf("Folder in file not found\n");
+    return 2;
+  }
   
   TTree *test_tree  = (TTree*) f->Get("TestTree");
   TTree *train_tree = (TTree*) f->Get("TrainTree");
@@ -295,13 +328,29 @@ int stack_tmva_tree(const char* file = "../CWoLa_training_background_3.root", do
 int plot_roc(const char* file = "../CWoLa_training_background_3.root", int category = 15, double mva_step = 1.e-2) {
 
   if(setSilent_) gROOT->SetBatch(kTRUE);
-  TFile* f = new TFile(file,"READ");
+
+  TString fname = file;
+  fname = "../" + fname;
+  fname += "/";
+  fname += file;
+  fname += ".root";
+    
+  TFile* f = new TFile(fname.Data(),"READ");
   if(!f) {
     printf("File not found\n");
     return 1;
   }
-  TString fname = file;
 
+  if(new_training_) {
+    TString foldername = "tmva_";
+    foldername += file;
+    f = (TFile*) f->Get(foldername.Data());
+  }
+  if(!f)  {
+    printf("Folder in file not found\n");
+    return 2;
+  }
+  
   TTree *test_tree  = (TTree*) f->Get("TestTree");
   TTree *train_tree = (TTree*) f->Get("TrainTree");
   if(!test_tree || !train_tree) {
@@ -444,11 +493,27 @@ int plot_2D_tmva_tree(const char* file = "../CWoLa_training_background_3.root", 
   else gROOT->SetBatch(kFALSE);
 
   TString fname = file;
-  TFile* f = new TFile(file,"READ");
+  fname = "../" + fname;
+  fname += "/";
+  fname += file;
+  fname += ".root";
+    
+  TFile* f = new TFile(fname.Data(),"READ");
   if(!f) {
     printf("File not found\n");
     return 1;
   }
+
+  if(new_training_) {
+    TString foldername = "tmva_";
+    foldername += file;
+    f = (TFile*) f->Get(foldername.Data());
+  }
+  if(!f)  {
+    printf("Folder in file not found\n");
+    return 2;
+  }
+  
   TTree *test_tree  = (TTree*) f->Get("TestTree");
   TTree *train_tree = (TTree*) f->Get("TrainTree");
   if(!test_tree || !train_tree) {
@@ -521,6 +586,217 @@ int plot_2D_tmva_tree(const char* file = "../CWoLa_training_background_3.root", 
       fnm += "_noCat_15";
     c->Print(Form("figures/%s_2D_hist_%s_%s_%.4f.png", fnm.Data(), var_.Data(), mva_var.Data(), mva_cut));
   }
+  return 0;
+}
+
+int plot_correlations_tmva_tree(const char* file = "CWoLa_training_background_3", int category = -1) {
+
+  // Use TH2::GetCorrelationFactor
+  if(setSilent_) gROOT->SetBatch(kTRUE);
+  else gROOT->SetBatch(kFALSE);
+
+  TString fname = file;
+  fname = "../" + fname;
+  fname += "/";
+  fname += file;
+  fname += ".root";
+    
+  TFile* f = new TFile(fname.Data(),"READ");
+  if(!f) {
+    printf("File not found\n");
+    return 1;
+  }
+
+  if(new_training_) {
+    TString foldername = "tmva_";
+    foldername += file;
+    f = (TFile*) f->Get(foldername.Data());
+  }
+  
+  if(!f)  {
+    printf("Folder in file not found\n");
+    return 2;
+  }
+  
+  TTree *test_tree  = (TTree*) f->Get("TestTree");
+  TTree *train_tree = (TTree*) f->Get("TrainTree");
+  if(!test_tree || !train_tree) {
+    printf("Trees not found\n");
+    return 2;
+  }
+
+  vector<double>  corrs;
+  vector<TString> names;
+  auto branches = test_tree->GetListOfBranches();
+  int numB = branches->GetSize();
+  for(int br = 0; br < numB; ++br) {  
+  
+    TH2F* htest; //  = new TH2F("htest" ,"Test" ,bins_,xMin_,xMax_, binsy_, yMin_, yMax_);
+    TH2F* htrain; // = new TH2F("htrain","Train",bins_,xMin_,xMax_, binsy_, yMin_, yMax_);
+
+    TString mva_var;
+    if(MVA_ == 0)
+      mva_var = "MLP_MM";
+    else if(MVA_ == 1)
+      mva_var = "BDT";
+    else if(MVA_ == 2)
+      mva_var = "BDTRT";
+    else {
+      printf("MVA_ value doesn't correspond to known MVA\n");
+      return 3;
+    }
+  
+    TString cut;
+    if(fname.Contains("mock"))
+      cut = "genWeight"; //don't weight data/mock data
+    else
+      cut = "fullEventWeight";
+    
+    if(category > -1) cut += Form("*(eventCategory == %i)",category);
+    if(category < -1) cut += Form("*(eventCategory != %i)",category);
+
+    TString var = (branches->At(br))->GetName();
+    test_tree-> Draw(Form("%s:%s>>htest" , var.Data(), mva_var.Data()), cut.Data());
+    htest = (TH2F*) gDirectory->Get("htest");
+    htrain = (TH2F*) htest->Clone("htrain");
+    htrain->Reset();
+    train_tree->Draw(Form("%s:%s>>htrain", var.Data(), mva_var.Data()), cut.Data());
+
+    TObject* o = (gDirectory->Get("c1"));
+    delete o;
+
+    htrain->Add(htest);
+    corrs.push_back(htrain->GetCorrelationFactor());
+    names.push_back(var);
+
+    printf("Correlation between %s and %s = %.6f\n", mva_var.Data(), var.Data(), corrs[br]);
+  }
+
+  return 0;
+}
+
+int plot_all_correlations_tmva_tree(const char* file = "CWoLa_training_background_3", int category = 15) {
+
+  // Use TH2::GetCorrelationFactor
+  if(setSilent_) gROOT->SetBatch(kTRUE);
+  else gROOT->SetBatch(kFALSE);
+
+  TString fname = file;
+  fname = "../" + fname;
+  fname += "/";
+  fname += file;
+  fname += ".root";
+    
+  TFile* f = new TFile(fname.Data(),"READ");
+  if(!f) {
+    printf("File not found\n");
+    return 1;
+  }
+
+  if(new_training_) {
+    TString foldername = "tmva_";
+    foldername += file;
+    f = (TFile*) f->Get(foldername.Data());
+  }
+  
+  if(!f)  {
+    printf("Folder in file not found\n");
+    return 2;
+  }
+  
+  TTree *test_tree  = (TTree*) f->Get("TestTree");
+  TTree *train_tree = (TTree*) f->Get("TrainTree");
+  if(!test_tree || !train_tree) {
+    printf("Trees not found\n");
+    return 2;
+  }
+
+  vector<TString> names;
+  auto branches = test_tree->GetListOfBranches();
+  int numB = branches->GetSize();
+
+  TH2F* corrMatB = new TH2F("corrMatB", "Correlation Matrix (background)", numB, 0, numB, numB, 0, numB);
+  TH2F* corrMatS = new TH2F("corrMatS", "Correlation Matrix (signal)"    , numB, 0, numB, numB, 0, numB);
+
+  gROOT->SetBatch(kTRUE);
+  for(int brx = 0; brx < numB; ++brx) {  
+    TString varx = (branches->At(brx))->GetName();
+    corrMatB->GetXaxis()->SetBinLabel(brx+1, varx.Data());
+    corrMatB->GetYaxis()->SetBinLabel(brx+1, varx.Data());
+    corrMatS->GetXaxis()->SetBinLabel(brx+1, varx.Data());
+    corrMatS->GetYaxis()->SetBinLabel(brx+1, varx.Data());
+    corrMatS->SetBinContent(brx+1,brx+1, 100.);
+    corrMatB->SetBinContent(brx+1,brx+1, 100.);
+    for(int bry = brx+1; bry < numB; ++bry) {  
+  
+      TH2F* htest; 
+      TH2F* htrain; 
+
+      TString cut;
+      if(fname.Contains("mock"))
+	cut = "genWeight"; //don't weight data/mock data
+      else
+	cut = "fullEventWeight";
+    
+      TString cutS = cut + Form("*(eventCategory == %i)",category);
+      TString cutB = cut + Form("*(eventCategory != %i)",category);
+
+      TString vary = (branches->At(bry))->GetName();
+
+      test_tree-> Draw(Form("%s:%s>>htest" , varx.Data(), vary.Data()), cutB.Data());
+      htest = (TH2F*) gDirectory->Get("htest");
+      htrain = (TH2F*) htest->Clone("htrain");
+      htrain->Reset();
+      train_tree->Draw(Form("%s:%s>>htrain", varx.Data(), vary.Data()), cutB.Data());
+
+      htrain->Add(htest);
+      double corrB = 100.*htrain->GetCorrelationFactor();
+      corrMatB->SetBinContent(brx+1,bry+1,corrB);
+      corrMatB->SetBinContent(bry+1,brx+1,corrB);
+
+      test_tree-> Draw(Form("%s:%s>>htest" , varx.Data(), vary.Data()), cutS.Data());
+      htest = (TH2F*) gDirectory->Get("htest");
+      htrain = (TH2F*) htest->Clone("htrain");
+      htrain->Reset();
+      train_tree->Draw(Form("%s:%s>>htrain", varx.Data(), vary.Data()), cutS.Data());
+
+      htrain->Add(htest);
+      
+      double corrS = 100.*htrain->GetCorrelationFactor();
+      corrMatS->SetBinContent(brx+1,bry+1,corrS);
+      corrMatS->SetBinContent(bry+1,brx+1,corrS);
+      delete htest;
+      delete htrain;
+    }
+  }
+  if(!setSilent_) gROOT->SetBatch(kFALSE);
+
+  TCanvas* c1 = new TCanvas("corrsS", "Correlations (signal)", 750,900);
+  corrMatS->Draw("text colz");
+  c1->Update();
+  
+  corrMatS->GetXaxis()->SetLabelSize(0.02);
+  corrMatS->GetYaxis()->SetLabelSize(0.02);
+  TPaletteAxis* paletteAxis = (TPaletteAxis*)corrMatS->GetListOfFunctions()->FindObject( "palette" );
+  paletteAxis->SetLabelSize( 0.03 );
+  paletteAxis->SetX1NDC( paletteAxis->GetX1NDC() + 0.02 );
+  TCanvas* c2 = new TCanvas("corrsB", "Correlations (background)", 750,900);
+  corrMatB->Draw("text colz");
+  c2->Update();
+
+  paletteAxis = (TPaletteAxis*)corrMatB->GetListOfFunctions()->FindObject( "palette" );
+  paletteAxis->SetLabelSize( 0.03 );
+  paletteAxis->SetX1NDC( paletteAxis->GetX1NDC() + 0.02 );
+  gStyle->SetOptStat(0);
+  gStyle->SetPaintTextFormat( ".0f" );
+
+  corrMatB->GetXaxis()->SetLabelSize(0.02);
+  corrMatB->GetYaxis()->SetLabelSize(0.02);
+  c1->Modified();
+  c1->Update();
+  c2->Modified();
+  c2->Update();
+  
   return 0;
 }
 
