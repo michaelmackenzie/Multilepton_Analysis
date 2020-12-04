65f270c22ca8dbada8184bbaf6102a0e50c0d611
michaelmackenzie <michaelmackenzie@u.northwestern.edu>
2020-12-04 15:57:38 -0600

    Recalculate b-tag efficiencies

---
+++ studies/btag_scale/make_all_scales.sh
@@ -0,0 +1,21 @@
+#! /bin/bash
+
+years="2016 2017 2018"
+wps="0 1 2"
+if [[ "$1" == "" ]]
+then
+    selection="mumu"
+else
+    selection="$1"
+fi
+for year in $years
+do
+    echo "Making scale factors for year = ${year}"
+    for wp in $wps
+    do
+	root.exe -q -b "scale_factors.C(\"${selection}\", 7, ${year}, ${wp})"
+    done
+done
+
+echo "Finished!"
+    
