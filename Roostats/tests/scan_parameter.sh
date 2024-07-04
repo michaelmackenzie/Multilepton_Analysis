31c8b73df3523026a163478a7495f7cc6b01d25f
Michael MacKenzie <michael.mackenzie@cern.ch>
2024-07-04 15:59:43 +0200

    Add optional random seed to parameter scan

--- Roostats/tests/scan_parameter.sh
+++ Roostats/tests/scan_parameter.sh
@@ -8,6 +8,7 @@ Help() {
     echo " 3: Fit arguments"
     echo " 4: N(points) (default = 50)"
     echo " 5: Plot only"
+    echo " 6: Random seed"
 }
 
 CARD=$1
@@ -15,6 +16,7 @@ PARA=$2
 ARGS=$3
 POINTS=$4
 PLOTONLY=$5
+SEED=$6
 
 if [[ "${CARD}" == "-h" ]] || [[ "${CARD}" == "--help" ]] || [[ "${CARD}" == "" ]]; then
     Help
@@ -36,12 +38,18 @@ fi
 
 if [[ "${PLOTONLY}" == "" ]]; then
     echo "Scanning parameter ${PARA} using card ${CARD}"
-
+    if [[ "${SEED}" != "" ]]; then
+        ARGS="${ARGS} -s ${SEED}"
+    fi
     echo ">>> combine -d ${CARD} -M MultiDimFit --saveNLL --algo grid -P ${PARA} --redefineSignalPOIs r --floatOtherPOIs 1 --points ${POINTS} ${ARGS}"
     combine -d ${CARD} -M MultiDimFit --saveNLL --algo grid -P ${PARA} --redefineSignalPOIs r --floatOtherPOIs 1 --points ${POINTS} ${ARGS}
 fi
 
 FILE="higgsCombineTest.MultiDimFit.mH120.root"
+if [[ "${SEED}" != "" ]]; then
+    FILE="higgsCombineTest.MultiDimFit.mH120.${SEED}.root"
+fi
+
 
 if [ ! -f ${FILE} ]; then
     echo "File ${FILE} not found!"
